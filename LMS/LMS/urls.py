a632da894a2cc1759fab95194f9d700c853d443d
Devendra <ysdevn@gmail.com>
2024-01-18 05:58:36 +0530

    2nd on18jan

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Registeration.views import index
+from Registeration.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index)
+    path('',index,name='home'),
+    path('accounts/',accounts,name="accounts")
 ]
